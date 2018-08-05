6e031ca9f8d1db9801f17c46457d3c485bf0ec09
GameAboutThings <lyfegame@gmail.com>
2018-08-05 17:38:57 +0200

    Some more work on the Modular AI System

--- Lyfe_Game/Source/Lyfe_Game/Private/AI_Node.cpp
+++ Lyfe_Game/Source/Lyfe_Game/Private/AI_Node.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AI_Node.h"
+#include "Logging.h"
 
 //+++++++++++++++++++++++++++++++++++++++++++ AI_NODE +++++++++++++++++++++++++++++++
 
@@ -12,6 +13,15 @@ AI_Node::~AI_Node()
 {
 }
 
+//+++++++++++++++++++++++++++++++++++++++++++ AI_BASENODE +++++++++++++++++++++++++++++++
+
+AI_BaseNode::AI_BaseNode()
+{
+}
+
+AI_BaseNode::~AI_BaseNode()
+{
+}
 
 //+++++++++++++++++++++++++++++++++++++++++++ AI_DECISIONNODE +++++++++++++++++++++++++++++++
 
@@ -21,6 +31,80 @@ AI_DecisionNode::AI_DecisionNode()
 
 AI_DecisionNode::~AI_DecisionNode()
 {
+}
+
+void AI_DecisionNode::MakeDecision(AGameMode * gameMode)
+{
+	switch (_eLeftSide)
+	{
+	case EConditionLeftSide::ENothing: 
+	{ 
+		//Put something like "Default()" in here
+		//switch _eStem
+		Logging::Log("MakeDecision.ENothing not implemented yet!", true); 
+	}
+		break;
+	case EConditionLeftSide::EHealth: 
+	{ 
+		switch (_eMiddle)
+		{
+		case EConditionMiddle::EEmpty: { Logging::Log("MakeDecision.EHealth.EEmpty not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EGreater: { Logging::Log("MakeDecision.EHealth.EGreater not implemented yet!", true); }
+			break;
+		case EConditionMiddle::ESmaller: { Logging::Log("MakeDecision.EHealth.ESmaller not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EEqual: { Logging::Log("MakeDecision.EHealth.EEqual not implemented yet!", true); }
+			break;
+		}
+	}
+		break;
+	case EConditionLeftSide::ENutrition: 
+	{
+		switch (_eMiddle)
+		{
+		case EConditionMiddle::EEmpty: { Logging::Log("MakeDecision.ENutrition.EEmpty not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EGreater: { Logging::Log("MakeDecision.ENutrition.EGreater not implemented yet!", true); }
+			break;
+		case EConditionMiddle::ESmaller: { Logging::Log("MakeDecision.ENutrition.ESmaller not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EEqual: { Logging::Log("MakeDecision.ENutrition.EEqual not implemented yet!", true); }
+			break;
+		}
+	}
+		break;
+	case EConditionLeftSide::EAttacked: 
+	{ 
+		switch (_eMiddle)
+		{
+		case EConditionMiddle::EEmpty: { Logging::Log("MakeDecision.EAttacked.EEmpty not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EGreater: { Logging::Log("MakeDecision.EAttacked.EGreater not implemented yet!", true); }
+			break;
+		case EConditionMiddle::ESmaller: { Logging::Log("MakeDecision.EAttacked.ESmaller not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EEqual: { Logging::Log("MakeDecision.EAttacked.EEqual not implemented yet!", true); }
+			break;
+		}
+	}
+		break;
+	case EConditionLeftSide::EEnemy: 
+	{
+		switch (_eMiddle)
+		{
+		case EConditionMiddle::EEmpty: { Logging::Log("MakeDecision.EEnemy.EEmpty not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EGreater: { Logging::Log("MakeDecision.EEnemy.EGreater not implemented yet!", true); }
+			break;
+		case EConditionMiddle::ESmaller: { Logging::Log("MakeDecision.EEnemy.ESmaller not implemented yet!", true); }
+			break;
+		case EConditionMiddle::EEqual: { Logging::Log("MakeDecision.EEnemy.EEqual not implemented yet!", true); }
+			break;
+		}
+	}
+		break;
+	}
 }
 
 //+++++++++++++++++++++++++++++++++++++++++++ AI_ACTIONNODE +++++++++++++++++++++++++++++++
--- Lyfe_Game/Source/Lyfe_Game/Public/AI_Node.h
+++ Lyfe_Game/Source/Lyfe_Game/Public/AI_Node.h
@@ -6,10 +6,28 @@
 
 enum class EConditionLeftSide : uint8
 {
+	ENothing,
 	EHealth,
 	ENutrition,
 	EAttacked,
-	EEnemy
+	EEnemy //used for enemy nearby or something
+};
+
+enum class EConditionMiddle : uint8
+{
+	EEmpty,
+	EGreater,
+	ESmaller,
+	EEqual
+};
+
+enum class EConditionStem : uint8
+{
+	EIdle,
+	EAttacked,
+	ELowHealth,
+	ELowNutrition,
+	ENight
 };
 
 /**
@@ -22,11 +40,37 @@ public:
 	~AI_Node();
 };
 
+class LYFE_GAME_API AI_BaseNode : AI_Node
+{
+public:
+	AI_BaseNode();
+	~AI_BaseNode();
+private:
+	AI_Node * idle;
+	AI_Node * attacked;
+	AI_Node * lowHealth;
+	AI_Node * lowNutrition;
+	AI_Node * night;
+public:
+	void AddIdleNode();
+};
+
 class LYFE_GAME_API AI_DecisionNode : AI_Node
 {
 public:
 	AI_DecisionNode();
 	~AI_DecisionNode();
+
+private:
+	TEnumAsByte<EConditionLeftSide> _eLeftSide;
+	TEnumAsByte<EConditionMiddle> _eMiddle;
+	TEnumAsByte<EConditionStem> _eStem;
+
+	AI_Node* leftChild;
+	AI_Node* rightChild;
+
+public:
+	void MakeDecision(class AGameMode* gameMode);
 };
 
 class LYFE_GAME_API AI_ActionNode : AI_Node
